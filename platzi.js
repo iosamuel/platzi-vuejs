b5be47c142b894932e886e2649ac047ab5424df3
Samuel BR <samuelb1311@gmail.com>
2020-11-07 18:56:13 -0500

    Reflect

--- platzi.js
+++ platzi.js
@@ -9,13 +9,19 @@ class PlatziReactive {
     // $data = destino
     this.$data = new Proxy(origen, {
       get(target, name) {
-        if (name in target) {
+        /* if (name in target) {
           return target[name];
+        } */
+        if (Reflect.has(target, name)) {
+          return Reflect.get(target, name);
         }
         console.warn("La propiedad que intentas acceder no existe");
         return "";
       },
-      set() {}
+      set(target, name, value) {
+        console.log("Cambiando", name, "a", value);
+        Reflect.set(target, name, value);
+      }
     });
 
     this.mount();
@@ -25,11 +31,24 @@ class PlatziReactive {
     document.querySelectorAll("*[p-text]").forEach(el => {
       this.pText(el, this.$data, el.getAttribute("p-text"));
     });
+
+    document.querySelectorAll("*[p-model]").forEach(el => {
+      const attr = el.getAttribute("p-model");
+      el.value = Reflect.get(this.$data, attr);
+
+      el.addEventListener("input", () => {
+        this.pModel(el, this.$data, attr);
+      });
+    });
   }
 
   pText(el, target, name) {
-    const content = target[name];
+    const content = Reflect.get(target, name);
     el.innerText = content;
+  }
+
+  pModel(el, target, name) {
+    Reflect.set(target, name, el.value);
   }
 }
 
