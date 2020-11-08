2f4f792c37992d57383032ce51e2f58ebe01b87a
Samuel BR <samuelb1311@gmail.com>
2020-11-08 08:03:53 -0500

    Definicion

--- platzi.js
+++ platzi.js
@@ -4,33 +4,20 @@ class PlatziReactive {
       data() => { ... }
   */
   constructor({ data }) {
-    const origen = data();
-
-    // $data = destino
-    this.$data = new Proxy(origen, {
-      get(target, name) {
-        if (name in target) {
-          return target[name];
-        }
-        console.warn("La propiedad que intentas acceder no existe");
-        return "";
-      },
-      set() {}
-    });
-
-    this.mount();
+    this.origen = data();
   }
 
   mount() {
     document.querySelectorAll("*[p-text]").forEach(el => {
-      this.pText(el, this.$data, el.getAttribute("p-text"));
+      this.pText(el, this.origen, el.getAttribute("p-text"));
     });
   }
 
   pText(el, target, name) {
-    const content = target[name];
-    el.innerText = content;
+    el.innerText = target[name];
   }
+
+  pModel() {}
 }
 
 var Platzi = {
