401e5b629ab6b79c58bc3090d081cd83c4bf77d9
Samuel BR <samuelb1311@gmail.com>
2020-11-08 08:22:15 -0500

    Reflect

--- platzi.js
+++ platzi.js
@@ -1,36 +1,27 @@
 class PlatziReactive {
-  // Dependecias
-  deps = new Map();
-
   /*
     options:
       data() => { ... }
   */
   constructor({ data }) {
-    const origen = data();
+    this.origen = data();
 
-    const self = this;
-
-    // $data = destino
-    this.$data = new Proxy(origen, {
+    // Destino
+    this.$data = new Proxy(this.origen, {
       get(target, name) {
         /* if (name in target) {
           return target[name];
         } */
         if (Reflect.has(target, name)) {
-          self.track(target, name);
           return Reflect.get(target, name);
         }
-        console.warn("La propiedad que intentas acceder no existe");
+        console.warn("La propiedad", name, "no existe");
         return "";
       },
       set(target, name, value) {
         Reflect.set(target, name, value);
-        self.trigger(target, name);
       }
     });
-
-    this.mount();
   }
 
   mount() {
@@ -40,37 +31,20 @@ class PlatziReactive {
 
     document.querySelectorAll("*[p-model]").forEach(el => {
       const name = el.getAttribute("p-model");
-      el.value = Reflect.get(this.$data, name);
+      this.pModel(el, this.$data, name);
 
       el.addEventListener("input", () => {
-        this.pModel(el, this.$data, name);
+        Reflect.set(this.$data, name, el.value);
       });
     });
   }
 
-  track(target, name) {
-    if (!this.deps.has(name)) {
-      const effect = () => {
-        document.querySelectorAll(`*[p-text=${name}]`).forEach(el => {
-          this.pText(el, target, name);
-        });
-      };
-      this.deps.set(name, effect);
-    }
-  }
-
-  trigger(target, name) {
-    const effect = this.deps.get(name);
-    effect();
-  }
-
   pText(el, target, name) {
-    const content = Reflect.get(target, name);
-    el.innerText = content;
+    el.innerText = Reflect.get(target, name);
   }
 
   pModel(el, target, name) {
-    Reflect.set(target, name, el.value);
+    el.value = Reflect.get(target, name);
   }
 }
 
